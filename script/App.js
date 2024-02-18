765520cbccdad116341a2e0ceab814ec9f105844
unknown <mydulcse62.niter@gmail.com>
2024-02-18 19:35:20 +0600

    added functionality

---
+++ script/App.js
@@ -0,0 +1,106 @@
+const addedCardIds = [];
+addCounter()
+function addCounter() {
+  const buttons = document.getElementsByClassName('btn-add-card');
+  const begCount = document.getElementById('beg-count');
+  let count = parseInt(begCount.innerText);
+
+  for (const button of buttons) {
+    button.addEventListener('click', () => {
+      if (count < 4) {
+        const buttonId = button.innerText;
+        if (!addedCardIds.includes(buttonId)) {
+          count++;
+          begCount.innerText = count;
+          removeCounter();
+          addCard(buttonId);
+          totalCost(count);
+          addCuponanable();
+          button.classList.add("bg-[#1DD100]");
+          addedCardIds.push(buttonId);
+        } 
+      } else {
+        alert("Limit crossed");
+      }
+    });
+  }
+}
+function removeCounter(){
+  const totalSeat=document.getElementById('total_sets');
+  let seats = parseInt(totalSeat.innerText);
+  totalSeat.innerText = (seats-1);
+}
+
+function addCard(buttonId){
+  const cardHolher=document.getElementById('card-holder');
+  
+  cardHolher.innerHTML +=`<div  class="flex justify-between items-center gap-2 pt-8 pb-4 ">
+  <h2 class="text-[18px] font-semibold raleway text-[#03071299]">${buttonId}</h2>
+  <h2 class="text-[18px] font-semibold raleway text-[#03071299]">
+    Economoy
+  </h2>
+  <h2 class="text-[18px] font-semibold raleway text-[#03071299]">
+    550
+  </h2>
+</div>`
+}
+function totalCost(items){
+const TotalPrice = items * 550;
+const priceSelector = document.getElementById('total-price');
+const grandPriceCell = document.getElementById('grand-price')
+priceSelector.innerText =TotalPrice;
+grandPriceCell.innerText=TotalPrice;
+}
+// function setBackgroundColor(elementClass) {
+  
+// }
+function addCuponanable() {
+  const begCount=document.getElementById('beg-count');
+  let count = parseInt(begCount.innerText)
+  const cupon = document.getElementById('cupon');
+  if (count ==4) {
+    cupon.removeAttribute('disabled');
+  }
+
+
+}
+function addCupon() {
+  const cupon = document.getElementById('cupon').value;
+  const cuponId = document.getElementById('cuppon-section');
+  const showDiscount = document.getElementById('show-discount')
+  const newOffer = document.getElementById('offerFify').innerText;
+  const coupleOffer = document.getElementById('coupleTewnty').innerText;
+  let grandPriceCell = document.getElementById('grand-price');
+  let grandPrice = document.getElementById('grand-price').innerText;
+  const discount = document.getElementById('discount');
+
+
+  if (cupon === newOffer) {
+    NowGPrice = grandPrice -(grandPrice * 0.15)
+    grandPriceCell.innerText=NowGPrice;
+    discount.innerText=(grandPrice * 0.15);
+  } else if (cupon === coupleOffer) {
+    NowGPrice = grandPrice-(grandPrice * 0.20);
+    grandPriceCell.innerText=NowGPrice;
+    discount.innerText=(grandPrice * 0.20);
+  }else{
+    alert("invallid cupon")
+  }
+  cuponId.classList.add('hidden');
+showDiscount.classList.remove('hidden')
+}
+
+
+
+
+function modal(){
+  const begCount=document.getElementById('beg-count');
+  let count = parseInt(begCount.innerText)
+  console.log(count)
+  const phoneNumber = document.getElementById('Phone-number');
+  const phone =phoneNumber.value;
+  if(count>=1 && phone >0){
+    my_modal_5.showModal()
+  }
+  
+}
